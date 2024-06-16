0e758093a994236521717ad4d03696f9c3e69af9
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2024-06-16 11:19:42 +0530

    Create Patching array.java

---
+++ Leetcode/Patching array.java
@@ -0,0 +1,15 @@
+long miss = 1;
+        int result = 0;
+        int i = 0;
+
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                result++;
+            }
+        }
+
+        return result;
