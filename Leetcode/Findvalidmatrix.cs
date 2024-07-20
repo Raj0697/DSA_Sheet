3800648d66082641fb057970620cabe82bac7a42
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2024-07-20 15:55:01 +0530

    Create Findvalidmatrix.cs

---
+++ Leetcode/Findvalidmatrix.cs
@@ -0,0 +1,22 @@
+int m = rowSum.Length, n = colSum.Length;
+            int[][] grid = new int[m][];
+            int i =0,j=0;
+            for(int k = 0;k<grid.Length;k++)
+            {
+                grid[k] = new int[n];
+            }
+            while( i < m && j < n ){
+                if(rowSum[i]<colSum[j])
+                {
+                    grid[i][j] = rowSum[i];
+                    colSum[j] -= rowSum[i];
+                    i++;
+                }
+                else if(rowSum[i]>=colSum[j])
+                {
+                    grid[i][j] = colSum[j];
+                    rowSum[i] -= colSum[j];
+                    j++;
+                }
+            }
+            return grid;
