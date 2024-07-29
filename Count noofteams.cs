85f9871ef2874816e05f343c861cdaefc759665f
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2024-07-29 13:30:41 +0530

    Create Count noofteams.cs

---
+++ Count noofteams.cs
@@ -0,0 +1,19 @@
+int n = rating.Length, count = 0;
+        for(int i = 1; i < n - 1; i++)
+        {
+            int curr = rating[i], leftSmaller = 0, leftBigger = 0, rightSmaller = 0, rightBigger = 0;
+            for(int l = 0; l < i; l++)
+            {
+                if(rating[l] < curr) leftSmaller++;
+                if(rating[l] > curr) leftBigger++;
+            }
+            
+            for(int r = i + 1; r < n; r++)
+            {
+                if(rating[r] < curr) rightSmaller++;
+                if(rating[r] > curr) rightBigger++;
+            }
+            
+            count += (leftSmaller * rightBigger) + (leftBigger * rightSmaller);
+        }
+        return count;
