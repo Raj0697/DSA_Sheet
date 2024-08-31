b7489dcf02e7936681868451cad62859a20390e7
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2024-08-31 21:54:14 +0530

    Update Concatenation of Consecutive Binary Numbers.java

--- Concatenation of Consecutive Binary Numbers.java
+++ Concatenation of Consecutive Binary Numbers.java
@@ -1,11 +1,11 @@
 class Solution {
     // TC - O(n)
-    public int concatenatedBinary(int n) {
+    public int concatenatedBinary(int nn) {
         int MOD = 1_000_000_007;
         long sum = 0;
         int length = 0;
         
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1; i <= nn; i++) {
             if((i & (i - 1)) == 0)
                 length++;
             sum = ((sum << length) | i) % MOD;
